5c866437789134d5ad75dfb5c3ae7bfbaac93b31
beleon <beleonattila@gmail.com>
2021-12-07 03:22:50 +0100

    Delete LIBS folder (WEKA files)

--- LIBS/sac/classifiers/sacTrain_OneClassClassifier_Weka.m
+++
@@ -1,8 +0,0 @@
-function classifier = sacTrain_OneClassClassifier_Weka(data, parameters)
-
-classifierWekaType = weka.classifiers.meta.OneClassClassifier;
-pa2 = '-tcl class2';
-
-filestr = mfilename();
-classifier = sacWekaTrain(classifierWekaType, data, pa2, filestr);
-
